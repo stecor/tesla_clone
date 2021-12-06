83746fc43687243607f87ce3a310dcac68c78a1e
Stefano Corra <stefano.corra@hotmail.com>
2021-12-05 21:15:05 -0500

    commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+function Home() {
+    return (
+        <Container>
+            Home
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+height: 100vh;
+`

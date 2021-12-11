2184926d143fa631b8d6a200cf69efd8e66a9132
Stefano Corra <stefano.corra@hotmail.com>
2021-12-11 10:20:07 -0500

    commit first page

--- src/components/Section.js
+++ src/components/Section.js
@@ -16,6 +16,7 @@ function Section() {
                 Existing Inventory
             </RightButton>
           </ButtonGroup>
+        <DownArrow src='/images/down-arrow.svg'/>
         </Wrap>
     )
 }
@@ -29,6 +30,10 @@ const Wrap = styled.div`
  background-position:center;
  background-repeat: not-repeat;
  background-image: url('images/model-s.jpg');
+ display: flex;
+ flex-direction: column;
+ justify-content: space-between; //vertical alignment
+ align-items: center; //horizontal alignment
 `
 
 const ItemText = styled.div`
@@ -37,13 +42,29 @@ const ItemText = styled.div`
 `
 
 const ButtonGroup = styled.div`
-
+ display: flex;
+ margin-bottom: 30px;
 `
 const LeftButton = styled.div`
   background-color: rgba(23,26,32,0.8);
   height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
 `
 
 const RightButton = styled(LeftButton)`
 
+`
+
+const DownArrow = styled.img`
+  margin-top:20px;
+  height:40px;
 `

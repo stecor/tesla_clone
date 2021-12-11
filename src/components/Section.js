6f70033a2ccac856a451f228c79f8fd666a8ae7b
Stefano Corra <stefano.corra@hotmail.com>
2021-12-11 09:22:42 -0500

    commit

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Section() {
     return (
-        <div>
+        <Wrap>
             Section
-        </div>
+        </Wrap>
     )
 }
 
 export default Section
+
+const Wrap = styled.div`
+ width:100vw;
+ height:100vh;
+ background-size:cover;
+ background-image: url('images/model-s.jpg');
+`

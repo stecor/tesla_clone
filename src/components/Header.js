0fbe210f40ded3f1bd85327ae28001ee9fc1c03c
Stefano Corra <stefano.corra@hotmail.com>
2021-12-16 20:08:38 -0500

    redux for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ import { useSelector } from 'react-redux'
 function Header() {
   const [burgerStatus, setBurgerStatus] = useState(false);
   const cars = useSelector(selectCars);
-  console.log(cars);
+
 
     return (
         <Container>
